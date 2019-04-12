8ad636323297c010c747e8a8bbd9b748a66de8d4
lshemanski <49000756+lshemanski@users.noreply.github.com>
2019-04-12 09:38:24 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-uBmYQmOF6oZaB-q15oo0trOhnnn-TYS8W-OBz5Hlyo/edit#gid=0';
